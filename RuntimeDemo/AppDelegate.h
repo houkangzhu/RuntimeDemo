24dac45286009aa454d31a1de90cabc11a029383
604952096@qq.com <houkangzhu@hotmail.com>
2016-04-22 13:20:02 +0800

    Initial Commit

---
+++ RuntimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeDemo
+//
+//  Created by Company on 16/4/22.
+//  Copyright © 2016年 侯康柱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeDemo
+//
+//  Created by Company on 16/4/22.
+//  Copyright © 2016年 侯康柱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
